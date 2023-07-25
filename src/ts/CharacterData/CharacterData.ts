49d698441bf06f27e4db8dced050def500e07a3f
Joan Pérez <joanperez.lapuente@gmail.com>
2023-07-25 16:41:57 +0200

    Create CharactersData.ts and types.ts

---
+++ src/ts/CharacterData/CharacterData.ts
@@ -0,0 +1,34 @@
+export const joffreyCharacterData = {
+  name: "Joffrey",
+  family: "Baratheon",
+  age: 16,
+  imageSource: "joffrey.jpg",
+};
+
+export const jaimeCharacterData = {
+  name: "Jaime",
+  family: "Lannister",
+  age: 45,
+  imageSource: "jaime.jpg",
+};
+
+export const daenerysCharacterData = {
+  name: "Daenerys",
+  family: "Targaryen",
+  age: 30,
+  imageSource: "daenerys.jpg",
+};
+
+export const tyrionCharacterData = {
+  name: "Tyrion",
+  family: "Lannister",
+  age: 40,
+  imageSource: "tyrion.jpg",
+};
+
+export const bronnCharacterData = {
+  name: "Bronn",
+  family: "Blackwater",
+  age: 54,
+  imageSource: "bronn.jpg",
+};
